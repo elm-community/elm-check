bb7b7056dc7daf4e618db67df55a288f56aea82b
Richard Feldman <richard.t.feldman@gmail.com>
2016-08-09 00:14:30 -0700

    Upgrade to use elm-test 2.0.0

--- src/Check/Test.elm
+++ src/Check/Test.elm
@@ -9,33 +9,36 @@ module Check.Test exposing (evidenceToTest)
 -}
 
 import Check
-import ElmTest
+import Test exposing (Test)
+import Expect
 
 
 {-| Convert elm-check's Evidence into an elm-test Test. You can use elm-test's
 runners to view the results of your property-based tests, alongside the results
 of unit tests.
 -}
-evidenceToTest : Check.Evidence -> ElmTest.Test
+evidenceToTest : Check.Evidence -> Test
 evidenceToTest evidence =
     case evidence of
         Check.Multiple name more ->
-            ElmTest.suite name (List.map evidenceToTest more)
+            Test.describe name (List.map evidenceToTest more)
 
         Check.Unit (Ok { name, numberOfChecks }) ->
-            ElmTest.test (name ++ " [" ++ nChecks numberOfChecks ++ "]") ElmTest.pass
+            Test.test (name ++ " [" ++ nChecks numberOfChecks ++ "]") <|
+                \() -> Expect.pass
 
         Check.Unit (Err { name, numberOfChecks, expected, actual, counterExample }) ->
-            ElmTest.test name
-                <| ElmTest.fail
-                <| "\nOn check "
-                ++ toString numberOfChecks
-                ++ ", found counterexample: "
-                ++ counterExample
-                ++ "\nExpected:   "
-                ++ expected
-                ++ "\nBut It Was: "
-                ++ actual
+            Test.test name <|
+                \() ->
+                    Expect.fail <|
+                        "\nOn check "
+                            ++ toString numberOfChecks
+                            ++ ", found counterexample: "
+                            ++ counterExample
+                            ++ "\nExpected:   "
+                            ++ expected
+                            ++ "\nBut It Was: "
+                            ++ actual
 
 
 nChecks : Int -> String
